277ea1f018e1feea170f9d5e03f2429b9f39e50c
Wei Zheng <zhengwei92@outlook.com>
2020-03-28 21:57:20 +0800

    added new notebook
    added new notebook

--- utils.py
+++ utils.py
@@ -1,66 +1,113 @@
-from keras.preprocessing import image
 import matplotlib.pyplot as plt
-from keras.applications.vgg16 import preprocess_input
-import keras.backend as K
 import numpy as np
 import cv2
 
-def plot_compare(imgs1, imgs2):
+def plot_compare(imgs1, imgs2, imgs3, labels):
     """
     Plot two lists of images side by side to compare visualization
     Args:
-        imgs1: Original images as np array
-        imgs2: XAI images as np array
+        imgs1: Original images as a list of np array
+        imgs2: XAI images as a list of np array
+        imgs3: XAI images highlights as a list of np.array
+        labels: top 5 predicted labels for each image
     """
     assert len(imgs1) == len(imgs2), 'The length of 2 inputs must be the same!'
     length = len(imgs1)
-    f, ax = plt.subplots(length, 2)
+    f, ax = plt.subplots(length, 4, figsize = (10, len(imgs1) * 2), dpi = 300)
     for i in range(length):
-        ax[i, 0].imshow(imgs1[i])
-        ax[i, 0].axis('off')
-        ax[i, 1].imshow(imgs2[i])
+        ax[i, 1].imshow(imgs1[i])
         ax[i, 1].axis('off')
+        ax[i, 2].imshow(imgs2[i])
+        ax[i, 2].axis('off')
+        ax[i, 3].imshow(imgs3[i])
+        ax[i, 3].axis('off')
+        label = [x[1] for x in labels[i]]
+        w = [x[-1] for x in labels[i]]
+        ax[i, 0].barh(np.arange(len(labels[i]), 0, -1), w, tick_label = label)
         if i == 0:
-            ax[i, 0].set_title('original')
-            ax[i, 1].set_title('XAI images')
-    f.suptitle('Comparison')
+            ax[i, 1].set_title('original')
+            ax[i, 2].set_title('XAI images')
+            ax[i, 3].set_title('top pixels')
+    f.suptitle('Comparison', fontsize=20)
     plt.show()
     return
 
-def overlay(img, mask, mode = 'mask'):
+def overlay(img, mask):
     """
     Visualize img and mask together in 1 picture.
     Args:
-        img: the original image as np array
-        mask: the binary mask produced by integrated gradients
-        mode: either 'mask' or 'highlight'
+        img: the original image as np array of shape (h, w, 3)
+        mask: the mask of shape(h, w)
     Returns:
         An image with mask overlay
     """
-    if mode not in ['mask', 'highlight']:
-        raise ValueError("mode must be either 'mask' or 'highlight'")
-    if mode == 'mask':
-        img = cv2.bitwise_and(img, img, mask = mask)
-    else:
+    if mask.dtype == np.float32:
+        assert (mask.max() <= 1) & (mask.min() >= 0), 'the mask should be a float between 0 and 1'
+        mask = np.expand_dims(mask, axis = -1)
+        img = img.astype(np.float32)
+        img = (img * mask).astype(np.uint8)
+    elif mask.dtype == np.uint8:
         x, y = np.where(mask != 0)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        img[x, y, :] = np.array([255, 0, 0])
+        img = img.copy()
+        img[x, y, :] = (np.array([0, 255, 0]))
+        img = img.astype(np.uint8)
     return img
 
-def normalize(img):
+def normalize(gradient,
+              high_percentile = 99.9,
+              low_percentile = 70,
+              high_bound = 1,
+              low_bound = 0.2):
     """
-    Normalize a floating point array to unsign 8-bit image
+    Normalize a floating point array range ~ [0, 1]
+    Args:
+        gradient: the aggregated gradient of shape (h, w)
+        high_percentile: upper bound of percentile cap
+        low_percentile: lower bound of percentile cap
+        high_bound: where to map the upper percentile cap
+        low_bound: where to map the lower percentile cap
+    Returns:
+        scale: a float array range ~ [low_bound, high_bound] of shape(h, w)
     """
-    img = (img - img.min()) / (img.max() - img.min()) * 255
-    return img.astype(np.uint8)
+    assert (high_bound <= 1) & (low_bound >= 0), 'The scale range is [0, 1]'
+    gradient_flat = gradient.reshape(-1)
+    high_thres = np.percentile(gradient, high_percentile)
+    low_thres = np.percentile(gradient, low_percentile)
+    gradient = np.clip(gradient, low_thres, high_thres)
+    # scale to between 0 and 1
+    gradient = (gradient - gradient.min()) / (gradient.max() - gradient.min())
+    # shift it to range (low_bound, high_bound)
+    scale = gradient * (high_bound - low_bound) + low_bound
+    return scale
 
-def selectTopPercent(img, percent = 0.05):
+def selectTopPercent(gradient, percent = 0.1):
     """
     Select the top x percent pixels in the integrated gradient image
     Args:
-        img: the integrated gradient image
+        gradient: the integrated gradient image of shape(h, w)
         percent: float between 0 and 1
     Returns:
-        
+        mask: a mask that indicates the top x percent pixel attribution
     """
+    grad_flat = gradient.reshape(-1)
+    percent = (1 - percent) * 100
+    thres = np.percentile(grad_flat, percent)
+    x, y = np.where(gradient >= thres)
+    mask = np.zeros(gradient.shape).astype(np.uint8)
+    mask[x, y] = 255
+    return mask
+
+def plot_sbs(img1, img2):
+    f, ax = plt.subplots(1, 2)
+    ax[0].imshow(img1)
+    ax[0].axis('off')
+    ax[1].imshow(img2)
+    ax[1].axis('off')
+    f.suptitle('input vs. baseline')
+    plt.show()
+
+def visualize(ig, img):
+    binary_mask = selectTopPercent(ig)
+    ovl = overlay(img, ig)
+    highlight = overlay(img, binary_mask)
+    return binary_mask, ovl, highlight
